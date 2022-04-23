4502e0b176fd0bce653ba2f239d1623d34dbf712
navneetset <navneetset@gmail.com>
2022-04-24 01:06:36 +0200

    42 basketball insights

---
+++ R-Course-KA/Section 4/42-Basketball Insights.R
@@ -0,0 +1,35 @@
+
+
+#Salary
+myplot(Salary)
+myplot(Salary/Games)
+myplot(Salary/FieldGoals)
+
+
+#In-game Metrics
+myplot(MinutesPlayed)
+myplot(Points)
+
+
+#In-game Metrics Normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+
+#Interesting Observations
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+
+#Time is valuable
+myplot(FieldGoals/MinutesPlayed)
+
+
+#Player style
+myplot(Points/FieldGoals)
+
+
+
+

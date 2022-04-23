bac51b460c16aabd0c232c2aa99f6bde85e0ce0f
navneetset <navneetset@gmail.com>
2022-04-23 20:16:27 +0200

    type of variables done

---
+++ R-Course-KA/Section 2/12-Types of variables.R
@@ -0,0 +1,25 @@
+
+#integer
+
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+
+#character
+a <- "h"
+typeof(a)
+
+
+#logical
+q <- T
+typeof(q)
+q2 <- FALSE
+typeof(q2)

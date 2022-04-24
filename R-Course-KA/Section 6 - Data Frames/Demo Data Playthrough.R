31a867652890c6a20326dcfc030d814d79182a7c
navneetset <navneetset@gmail.com>
2022-04-24 13:31:38 +0200

    renamed main file with everything in it

---
+++ R-Course-KA/Section 6 - Data Frames/Demo Data Playthrough.R
@@ -0,0 +1,61 @@
+library(dplyr)
+
+stats <- read.csv(file="Section 6 - Data Frames/P2-Demographic-Data.csv", stringsAsFactors = T)
+stats
+
+
+
+
+
+#------------------------------------ Exploring Dataset
+stats
+nrow(stats)
+ncol(stats)
+head(stats)
+glimpse(stats)
+tail(stats)
+str(stats) #str() runif()
+summary(stats)
+
+
+
+#------------------------------------ Using the $ sign
+stats
+head(stats)
+stats[3,"Birth.rate"]
+stats[3,3]
+stats$Internet.users
+stats$Internet.users[2]
+stats[,"Internet.users"]
+levels(stats$Income.Group)
+
+
+
+
+#------------------------------------- Basic Operations with a DF
+stats[1:10,] #subsetting
+stats[3:9,]
+stats[c(4,100),]
+
+#Remember how the [] work:
+is.data.frame(stats[1,]) #subsetting rows in dataframe remains as dataframes
+is.data.frame(stats[,1,drop=F]) #subsetting cols in dataframe drops dimensions by default
+#multiply columns
+head(stats)
+stats$Birth.rate * stats$Internet.users
+stats$Birth.rate + stats$Internet.users
+
+
+#add column to dataframe
+head(stats)
+stats$MyCalc <- stats$Birth.rate * stats$Internet.users #vectorised operation still
+
+#test of knowledge
+stats$xyz <- 1:5 #vector is recycled as long as its the same multiple of the rows
+head(stats, n=20)
+#remove a column
+head(stats)
+stats$MyCalc <- NULL
+stats$xyz <- NULL
+
+

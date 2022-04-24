5e63c7291f172863d9c69f7e465453f737d0ead2
navneetset <navneetset@gmail.com>
2022-04-24 13:37:11 +0200

    Finished 51

--- R-Course-KA/Section 6 - Data Frames/Demo Data Playthrough.R
+++ R-Course-KA/Section 6 - Data Frames/Demo Data Playthrough.R
@@ -59,3 +59,45 @@ stats$MyCalc <- NULL
 stats$xyz <- NULL
 
 
+
+
+
+#------------------------------------- Filtering Data Frames
+head(stats)
+filter <- stats$Internet.users < 2
+stats[stats$Internet.users < 2,]
+stats[filter,]
+
+
+stats[stats$Birth.rate > 40,]
+stats[stats$Birth.rate > 40 & stats$Internet.users < 2,]
+stats[stats$Income.Group == "High income",]
+levels(stats$Income.Group)
+
+
+stats[stats$Country.Name == "Malta",]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
